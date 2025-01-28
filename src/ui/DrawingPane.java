153d668c2b763a0b901278b08704c6611ca650c9
Nick <garnichtd@gmail.com>
2025-01-28 19:29:03 +0100

    Initial import

---
+++ src/ui/DrawingPane.java
@@ -0,0 +1,28 @@
+package ui;
+
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawingPane extends JPanel {
+
+    public DrawingPane(){
+        super();
+        setLayout(new BorderLayout());
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        System.out.println("paint...");
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0, this.getWidth(), this.getHeight() );
+
+        g.setColor(new Color(179, 255, 255));
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Stroke stroke = new BasicStroke(1, BasicStroke.CAP_BUTT,BasicStroke.JOIN_MITER);
+        g2d.setStroke(stroke);
+
+    }
+}
