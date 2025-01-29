05e67081b336e6f57130d1c06fdba4088b191316
Nick <garnichtd@gmail.com>
2025-01-29 08:31:10 +0100

    Drawing track lines

--- src/ui/DrawingPane.java
+++ src/ui/DrawingPane.java
@@ -23,6 +23,14 @@ public class DrawingPane extends JPanel {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         Stroke stroke = new BasicStroke(1, BasicStroke.CAP_BUTT,BasicStroke.JOIN_MITER);
         g2d.setStroke(stroke);
-        g2d.drawLine(0,0,100,100);
+        g2d.drawLine(0,80,2000,80);
+        g2d.drawLine(0,160,2000,160);
+        g2d.drawLine(0,240,2000,240);
+        g2d.drawLine(0,320,2000,320);
+        g2d.drawLine(0,400,2000,400);
+        g2d.drawLine(0,480,2000,480);
+        g2d.drawLine(0,560,2000,560);
+        g2d.drawLine(0,640,2000,640);
+        g2d.drawLine(0,720,2000,720);
     }
 }
